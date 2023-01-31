3f15ad5fea81c7b8743f43fd57252fba3b533599
quangvinh1102 <vinhls2002@gmail.com>
2023-01-31 20:16:50 +0700

    up code

--- resources/views/user/map-detail.blade.php
+++ resources/views/user/map-detail.blade.php
@@ -14,7 +14,7 @@
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Map Details</li>
-                    </ol>
+                    </ol> 
                 </nav>
             </div>
         </div>
