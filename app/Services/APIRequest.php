1850bbe16d06e22fc1c2d2a9618e7f01f8177692
LuongHoa <hoalv@rubicontechno.com>
2023-02-04 20:42:15 +0700

    call api weather

---
+++ app/Services/APIRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+use App\Services\ApiClient;
+class APIRequest extends ApiClient
+{
+    protected $client;
+
+    public function __construct(ApiClient $client)
+    {
+        $this->client = $client;
+    }
+    public function currentWeather($nameCyti) {
+        return  $this->client->get('http://api.weatherapi.com/v1/current.json', [
+            'key' => env('API_KEY_WEATHER'),
+            'q' => $nameCyti,
+            'lang' => 'vi',
+        ]);
+    }
+    public function dayWeather(){
+        return  $this->client->get('http://api.weatherapi.com/v1/forecast.json', [
+            'key' => env('API_KEY_WEATHER'),
+            'q' => '21.059945,105.719926',
+            'days' => 10,
+            'lang' => 'vi',
+        ]);
+    }
+    public function searchWeather(){
+        
+    }
+
+    public function currentAir($lat, $lon){
+        return $this->client->get('http://api.openweathermap.org/data/2.5/air_pollution', [
+            'lat' => $lat,
+            'lon' => $lon,
+            'appid' => env('API_KEY_AIR'),
+        ]);
+    }
+
+}
