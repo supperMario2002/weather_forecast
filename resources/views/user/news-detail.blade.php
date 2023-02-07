6360b3a7a9b3390f9fba8d253d63fe70dc9f22d8
quangvinh1102 <vinhls2002@gmail.com>
2023-02-07 20:54:25 +0700

    up code

--- resources/views/user/news-detail.blade.php
+++ resources/views/user/news-detail.blade.php
@@ -7,12 +7,12 @@
 
         <div class="container">
             <div class="d-md-flex align-items-center justify-content-between">
-                <h1 class="m-0"> News Details
+                <h1 class="m-0"> Tin tức
                 </h1>
                 <nav aria-label="breadcrumb m-0">
                     <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">News Details</li>
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Trang chủ</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Bài báo</li>
                     </ol>
                 </nav>
             </div>
@@ -26,10 +26,10 @@
             <div class="container">
                 <div class="row mb-5">
                     <div class="col-lg-8">
-                        <h2> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's</h2>
-                        <h5> By Brian Lada, Aircast meteorologist and staff writer</h5>
-                        <h6> Published Aug. 10, 2022 11:25 PM IST | Updated Aug. 15, 2022 4:40 PM IST </h6>
+                        <h2> Bắc Biển Đông có gió Đông Bắc giật cấp 9-10</h2>
+                        <h5> Bnews. Theo Trung tâm Dự báo khí tượng thủy văn quốc gia, ngày và đêm 1/2, Bắc Bộ và Bắc Trung
+                            Bộ có mưa, sương mù, trời rét về đêm và sáng sớm, vùng núi cao có nơi dưới 13 độ C.</h5>
+                        <h6> Cơ quan chủ quản: Thông tấn xã Việt Nam; Số 5 Lý Thường Kiệt - Hà Nội</h6>
                         <ul class="list-unstyled d-sm-flex">
                             <li>
                                 <a href="#">
@@ -52,41 +52,38 @@
                             <img src="images/pexels-photo-753619.jpg" alt="pm" />
                         </figure>
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        <p> Theo Trung tâm Dự báo khí tượng thuỷ văn quốc gia, sáng 29/1, khu vực Bắc Biển Đông có gió Đông
+                            Bắc mạnh cấp 6-7, giật cấp 9-10. Tại Trường Sa có gió Đông Bắc mạnh cấp 6-7, giật cấp 8-9.
+                            Ngày và đêm 29/1, khu vực Bắc và giữa Biển Đông (bao gồm cả vùng biển Hoàng Sa); khu vực phía
+                            Tây của Nam Biển Đông (bao gồm vùng biển phía Tây của Trường Sa) có gió Đông Bắc cấp 6-7, giật
+                            cấp 9-10, biển động mạnh, sóng biển cao 4-6m. Vùng biển từ Đà Nẵng đến Cà Mau có gió Đông Bắc
+                            cấp 6-7, giật cấp 8-9, biển động mạnh, sóng biển cao 3-5,5m.</p>
 
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
+
+                        <p> Ngày và đêm 30/1, khu vực Bắc và giữa Biển Đông (bao gồm vùng biển Hoàng Sa) và vùng biển phía
+                            Tây của khu vực Nam Biển Đông (bao gồm vùng biển phía Tây của Trường Sa), vùng biển từ Đà Nẵng
+                            đến Cà Mau gió Đông Bắc mạnh cấp 6, có lúc cấp 7, giật cấp 8-9, biển động mạnh; sóng biển cao
+                            4-6m, riêng khu vực Đà Nẵng đến Cà Mau sóng biển cao 3-6m.</p>
 
                         <figure class="body-img2">
                             <img src="images/pexels-photo-9541875.webp" alt="pm" />
                         </figure>
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        <p>Các tỉnh từ Thanh Hóa đến Thừa Thiên - Huế phía Bắc (Thanh Hóa và Nghệ An) ít mây, ngày nắng, đêm
+                            không mưa; phía Nam nhiều mây, ngày có mưa rải rác, đêm có mưa vài nơi. Gió Bắc đến Tây Bắc cấp
+                            3. Trời rét, phía Bắc có nơi rét đậm. Nhiệt độ thấp nhất 10-13 độ C, phía Nam 12-15 độ C. Nhiệt
+                            độ cao nhất 17-20 độ C. </p>
 
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        <p> Các tỉnh, thành phố từ Đà Nẵng đến Bình Thuận phía Bắc nhiều mây, ngày có mưa, mưa rào rải rác,
+                            đêm có mưa vài nơi; phía Nam có mây, ngày nắng, đêm có mưa rào vài nơi. Gió Đông Bắc cấp 3; vùng
+                            ven biển cấp 4-5, có nơi có gió giật mạnh cấp 6-7. Phía Bắc trời rét. Nhiệt độ thấp nhất 16-19
+                            độ C, phía Nam 20-23 độ C. Nhiệt độ cao nhất 19-22 độ C, phía Nam 25-28 độ C, có nơi trên 28 độ
+                            C. </p>
 
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                            the industry's
-                            standard dummy text ever since the 1500s, when an unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        <p>Nam Bộ ngày nắng, đêm có mưa vài nơi. Gió Đông Bắc đến Đông cấp 3. Sáng sớm và đêm trời lạnh. Nhiệt độ thấp nhất 20-23 độ C, miền Đông có nơi dưới 20 độ C. Nhiệt độ cao nhất 27-30 độ C, có nơi trên 30 độ C./. </p>
 
 
                     </div>
@@ -96,36 +93,36 @@
                             <h2> Topics </h2>
                             <ul class="list-unstyled mt-4">
                                 <li>
-                                    <a href="#"> <i class="fas fa-caret-right"></i> News & Features </a>
+                                    <a href="#"> <i class="fas fa-caret-right"></i> Tin tức & Nổi bật </a>
                                 </li>
                                 <li>
-                                    <a href="#"> <i class="fas fa-caret-right"></i> Astronomy </a>
+                                    <a href="#"> <i class="fas fa-caret-right"></i> Thiên văn </a>
                                 </li>
                                 <li>
-                                    <a href="#"> <i class="fas fa-caret-right"></i> Viral News</a>
+                                    <a href="#"> <i class="fas fa-caret-right"></i> Tin nổi tiếng</a>
                                 </li>
 
                             </ul>
                         </div>
 
                         <div class="resent-ct-new mt-5">
-                            <h2> Top News </h2>
+                            <h2> Bảng tin nổi bật </h2>
                             <a href="#" class="top-news">
 
-                                <h5>
-                                    Lorem Ipsum is simply dummy
+                                <h5 class="text-white"> 
+                                    Thời tiết bắc bộ
                                     <span class="d-block">
                                         2 days ago
                                     </span>
                                 </h5>
                                 <figure class="m-0">
-                                    <img src="images/pexels-genaro-serv%c3%83%c2%adn-763398.html" alt="ban" />
+                                    <img src="images/pexels-adhitya-andanu-1561806.jpg" alt="ban" />
                                 </figure>
                             </a>
                             <a href="#" class="top-news">
 
-                                <h5>
-                                    Lorem Ipsum is simply dummy
+                                <h5 class="text-white">
+                                    Thời tiết trung bộ
                                     <span class="d-block">
                                         1 days ago
                                     </span>
@@ -137,10 +134,10 @@
 
                             <a href="#" class="top-news">
 
-                                <h5>
-                                    Lorem Ipsum is simply dummy
+                                <h5 class="text-white">
+                                    Thời tiết nam bộ
                                     <span class="d-block">
-                                        2 days ago
+                                        3 days ago
                                     </span>
                                 </h5>
                                 <figure class="m-0">
@@ -149,8 +146,8 @@
                             </a>
                             <a href="#" class="top-news">
 
-                                <h5>
-                                    Lorem Ipsum is simply dummy
+                                <h5 class="text-white">
+                                    Thiên văn học
                                     <span class="d-block">
                                         2 days ago
                                     </span>
